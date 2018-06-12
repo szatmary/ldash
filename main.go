a87fd836d066df334bdf27184c87d26725e54390
Lei Zhang <lzhang3@snapchat.com>
2018-06-12 12:40:51 +0800

    support - in url format

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 		return
 	}
 
-	utils.GetMainLogger().Infof("baseDir", args[0])
+	utils.GetMainLogger().Infof("baseDir %s \n", args[0])
 
 	// clean the segment folder
 	utils.RemoveContents(args[0])
@@ -39,8 +39,8 @@ func main() {
 	//go utils.CheckExpire(args[0])
 
 	r := mux.NewRouter()
-	r.Handle("/ldash/upload/{name:[a-zA-Z0-9/._]+}", ldash_uploadHandler)
-	r.Handle("/ldash/download/{name:[a-zA-Z0-9/._]+}", ldash_downloadHandler)
+	r.Handle("/ldash/upload/{name:[a-zA-Z0-9/._-]+}", ldash_uploadHandler)
+	r.Handle("/ldash/download/{name:[a-zA-Z0-9/._-]+}", ldash_downloadHandler)
 
 	utils.GetMainLogger().Infof("start server\n")
 	log.Fatal(http.ListenAndServe(":8080", r))
