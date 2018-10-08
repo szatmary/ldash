b2146ff19f7594b2d671555caf396e9560321cb2
Ubuntu <ubuntu@ip-172-31-14-146.us-west-2.compute.internal>
2018-10-08 00:07:59 +0000

    add chunked download support

--- handlers/filedownloadhandler.go
+++ handlers/filedownloadhandler.go
@@ -6,7 +6,8 @@ import (
 	"os"
 	"path"
 	"time"
-
+        "strings"
+	"fmt"
 	"../utils"
 	"github.com/fujiwara/shapeio"
 )
@@ -36,40 +37,75 @@ func (l *FileDownloadHandler) serveDownload(w http.ResponseWriter, req *http.Req
 	}
 	defer file.Close()
 
-	utils.GetDownloadLogger().Debugf("file %s was downloaded @ %v \n", curFileURL, time.Now().Format(time.RFC3339))
+	utils.GetDownloadLogger().Debugf("file %s was requested @ %v \n", curFileURL, time.Now().Format(time.RFC3339))
 
-	w.Header().Set("Content-Type", "video/MP2T")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Transfer-Encoding", "chunked")
-	//w.Header().Set("Connection", "Keep-Alive")
+	w.Header().Set("Connection", "Keep-Alive")
+
+        if strings.HasSuffix(curFilePath, ".mpd") {
+                w.Header().Set("Content-Type", "application/dash+xml")
+        } else {
+        	w.Header().Set("Content-Type", "video/MP4")
+	}
 
 	w.WriteHeader(http.StatusOK)
-	writer := shapeio.NewWriter(w)
-	bufferSize := 2048
+	
+        writer := shapeio.NewWriter(w)
+
+	if strings.HasSuffix(curFilePath, ".mpd") { // we don't do chunkced download for manifest
+		utils.GetDownloadLogger().Debugf("download manifest file \n")
+		io.Copy(writer, file)
+		return
+	}
+
+	bufferSize := 20480
 
 	buffer := make([]byte, bufferSize)
-
+        var read_err error
+        bytesread := 0
 	for {
 		for {
-			bytesread, err := file.Read(buffer)
+			bytesread, read_err = file.Read(buffer)
+			if read_err != nil {
+                                 utils.GetDownloadLogger().Errorf("check: %v \n", read_err)
+				if read_err != io.EOF { // print out if read error
+					utils.GetDownloadLogger().Errorf("Failed to read file: %v \n", err)
+				}
+			}
+
+                        utils.GetDownloadLogger().Debugf("read %d bytes \n", bytesread)
 
 			if bytesread > 0 {
-				_, errpr := writer.Write(buffer[:bufferSize])
+				prefix := fmt.Sprintf("%d\r\n", bytesread)
+				init := []byte(prefix)
+				end := []byte("\r\n")
+				new_data := append(init, buffer...)
+				msg := append(new_data, end...)
+				_, errpr := writer.Write(msg)
 				if errpr != nil {
-					panic(err)
+					panic(errpr)
 				}
 			}
 
 			if bytesread != bufferSize {
+                                utils.GetDownloadLogger().Debugf("read all existing data \n")
 				break
 			}
 		}
 
-		if err != nil {
-			if err == io.EOF { // break out if reach to the file end
+		if read_err != nil {
+			if read_err == io.EOF { // break out if reach to the file end
 				break
 			}
 		}
 		time.Sleep(50 * time.Millisecond)
 	}
+
+	// write the end of chunk
+        msg := []byte("0\r\n\r\n")
+        _, errpr := writer.Write(msg)
+        if errpr != nil {
+          panic(errpr)
+        }
 }
